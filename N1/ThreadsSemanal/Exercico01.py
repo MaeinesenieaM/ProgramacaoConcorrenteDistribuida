d0d05a84fed246db5b5e9a886d56726618743aa8
MaeinesenieaM <david.costa.gomes71@gmail.com>
2025-03-22 10:07:31 -0300

    Terminado ExerciCO01

--- N1/ThreadsSemanal/Exercico01.py
+++ N1/ThreadsSemanal/Exercico01.py
@@ -1,18 +1,38 @@
 import random
+import threading
 
 # Função principal do QuickSort
+
+threads = []
+
+def sort_lesser(arr, target, pivot):
+    buffer = [x for x in arr[:-1] if x <= pivot]
+    target.extend(buffer)
+
+def sort_bigger(arr, target, pivot):
+    buffer = [x for x in arr[:-1] if x > pivot]
+    target.extend(buffer)
 
 def quicksort(arr):
     if len(arr) <= 1:
         return arr
-    pivot = arr[-1]
-    left = [x for x in arr[:-1] if x <= pivot]  # Elementos menores ou iguais ao pivô
-    right = [x for x in arr[:-1] if x > pivot]  # Elementos maiores que o pivô
+    pivot = arr[-1] #Usando um número negativo em uma array, ele conta de do fim ao início, aki ele sempre pega o último.
+
+    left = []
+    right = []
+    left_thread = threading.Thread(target = sort_lesser, args = (arr, left, pivot))
+    right_thread = threading.Thread(target = sort_bigger, args = (arr, right, pivot))
+
+    left_thread.start()
+    right_thread.start()
+    left_thread.join()
+    right_thread.join()
+
     return quicksort(left) + [pivot] + quicksort(right)
 
 # Função para gerar números aleatórios
 
-def gerar_numeros_aleatorios(n=100, min_val=1, max_val=200):
+def gerar_numeros_aleatorios(n=10, min_val=1, max_val=200):
     return [random.randint(min_val, max_val) for _ in range(n)]
 
 # Função principal para testar o QuickSort
